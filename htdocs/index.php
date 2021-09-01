972183b4524b26dc31400a2a15913e3c6089baac
Matthew McTague <matthew.mctague@vetta.nz>
2021-09-01 14:34:40 +1200

    debugging

--- htdocs/index.php
+++ htdocs/index.php
@@ -686,7 +686,7 @@ function process($url, $exec, $return_buffer = FALSE)
 			{
 				@shell_exec('echo n | '.$ssh_path.' '.implode(' ', $params).' screen-length 0 temporary');
 			}*/
-            
+
 			if ($fp = @popen('echo n | '.$ssh_path.' '.implode(' ', $params).' '.$exec, 'r'))
 			{
 				while (!feof($fp))
@@ -723,7 +723,7 @@ function process($url, $exec, $return_buffer = FALSE)
 					}
 				}
 
-				$pcloseresult = pclose($fp);
+				pclose($fp);
 			}
 
 			if (!$line)
